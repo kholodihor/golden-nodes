88f53fd0fa97ea8b5fb3400d1e00fc50fb431047
cold <kholodihor@gmail.com>
2025-12-25 15:43:32 +0100

    feat: add trpc

---
+++ src/app/client.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useTRPC } from "@/trpc/client"
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const Client = () => {
+  const trpc = useTRPC()
+  const { data } = useSuspenseQuery(trpc.hello.queryOptions({ text: "from Client" }));
+  return <div>{data.greeting}</div>
+}
